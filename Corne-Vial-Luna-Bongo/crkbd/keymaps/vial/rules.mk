ec1be015c1b3c7fec3cfb783c79d21cab9efda6f
Timothy Ryan <84595044+Timception@users.noreply.github.com>
2023-05-31 02:20:29 +0700

    Add files via upload

---
+++ Corne-Vial-Luna-Bongo/crkbd/keymaps/vial/rules.mk
@@ -0,0 +1,29 @@
+MOUSEKEY_ENABLE     = yes     # Mouse keys
+RGBLIGHT_ENABLE     = no      # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes     # Enable VIA
+VIAL_ENABLE         = yes     # Enable VIAL
+VIAL_INSECURE		= yes     # Enable/Disable VIAL Security
+LTO_ENABLE          = yes
+QMK_SETTINGS        = no
+COMBO_ENABLE        = no
+
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = yes
+WPM_ENABLE = yes
+
+
+#SPACE SAVING
+AUDIO_ENABLE			= no
+MUSIC_ENABLE			= no
+#CONSOLE_ENABLE			= no
+COMMAND_ENABLE			= no
+SPACE_CADET_ENABLE		= no
+#MAGIC_ENABLE			= no
+BLUETOOTH_ENABLE		= no		# Enable Bluetooth
+BACKLIGHT_ENABLE		= no		# Enable keyboard backlight functionality
+TAP_DANCE_ENABLE		= no
+KEY_OVERRIDE_ENABLE		= no
+EXTRAFLAGS				+= -flto
